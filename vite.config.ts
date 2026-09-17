3800e0a97a1d1a458b0c5effbe14048828e08961
Faisal Maqsood <59428054+faisalmaqsood0000@users.noreply.github.com>
2026-09-18 01:04:26 +0500

    Set base path for investment platform

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/investment-platform/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
